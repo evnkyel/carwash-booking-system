54432f34879e89d55d30b55cd877e7192f4d1528
evnkyel <eivancastro0801@gmail.com>
2025-10-03 19:33:42 +0800

    Week 1: Initial setup

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Car Wash Booking System</title>
+</head>
+<body>
+    <h1>Welcome to the Car Wash Booking System</h1>
+</body>
+</html>
