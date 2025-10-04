0f1d16c86ce8dd87a56c2fcc90c447a89b297e4e
evnkyel <eivancastro0801@gmail.com>
2025-10-04 11:27:56 +0800

    Updated config.php

--- config/config.php
+++ config/config.php
@@ -11,6 +11,6 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } else {
-        echo "✅ Database connected successfully!";
+        echo "Database connected successfully!";
     }
 ?>
